5f29a0c5878736485b8fcc6df878e7380c4cb37e
Bholu <bholutiwari121@gmail.com>
2021-08-03 11:39:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function myFunction1() {
+    //when I click on the bar I have to show sidebar
+    document.getElementById("sidebar").classList.add("sidebar-show");
+    //Initially bars have btn-show class so first we have to remove btn-show from bars and simultaneously add btn-hide on bars
+    //also add btn-show on cancel and remove btn-hide
+    document.getElementById("btn-on").classList.remove("btn-show");
+    document.getElementById("btn-off").classList.remove("btn-hide");
+    document.getElementById("btn-off").classList.add("btn-show");
+    document.getElementById("btn-on").classList.add("btn-hide");
+}
+
+function myFunction2() {
+     //when I click on cancel first I have to remove sidebar-show then add sidebarhide to remove sidebar
+    document.getElementById("sidebar").classList.remove("sidebar-show");
+    document.getElementById("sidebar").classList.add("sidebar-hide");
+    document.getElementById("btn-off").classList.remove("btn-show");
+    document.getElementById("btn-off").classList.add("btn-hide");
+    document.getElementById("btn-on").classList.add("btn-show");
+    document.getElementById("btn-on").classList.remove("btn-hide");
+}
